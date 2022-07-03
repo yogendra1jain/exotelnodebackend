62d57750ce9313389be587cf415a5f217140bcc3
Yogi <yogendrajain64@gmail.com>
2022-07-03 16:44:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,30 @@ app.get("/", (req, res) => {
     res.send("+917340299380");
 })
 
+app.get("/exotel", (req, res) => {
+    var query = req.query;
+    console.log(req.query);
+    res.json({
+    "fetch_after_attempt": false,
+    "destination_type": "external_number",
+    "destination": {
+        "device_contact_uri": "+917424926949"
+    },
+    "outgoing_phone_number": "01414931249",
+    "record": true,
+    "recording_channels": "dual",
+    "max_ringing_duration": 45,
+    "max_conversation_duration": 3600,
+    "music_on_hold": {
+        "type": "operator_tone"
+    },
+    "start_call_playback": {
+        "type": "text",
+        "value": "This text would be spoken out to the callee"
+    }
+});
+})
+
 app.listen(5000, () => {
     console.log("App started at port 5000");
 })
