64cd65075070478891c879a5eab099c6fcce7543
Yogi <yogendrajain64@gmail.com>
2022-07-03 17:07:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,23 +12,25 @@ app.get("/exotel", (req, res) => {
     var query = req.query;
     console.log(req.query);
     res.json({
-    "fetch_after_attempt": false,
-    "destination_type": "external_number",
-    "destination": {
-        "device_contact_uri": "+917424926949"
-    },
-    "outgoing_phone_number": "01414931249",
-    "record": true,
-    "recording_channels": "dual",
-    "max_ringing_duration": 45,
-    "max_conversation_duration": 3600,
-    "music_on_hold": {
-        "type": "operator_tone"
-    },
-    "start_call_playback": {
-        "type": "text",
-        "value": "This text would be spoken out to the callee"
-    }
+  "fetch_after_attempt": false,
+  "destination": {
+    "numbers": [
+      "+917424926949"
+    ]
+  },
+  "outgoing_phone_number": "01414931249",
+  "record": true,
+  "recording_channels": "dual",
+  "max_ringing_duration": 45,
+  "max_conversation_duration": 3600,
+  "music_on_hold": {
+    "type": "operator_tone"
+  },
+  "start_call_playback": {
+    "playback_to": "both",
+    "type": "text",
+    "value": "This text would be spoken out to the callee"
+  }
 });
 })
 
