4d027c967a607d2a7f952cd592f011076c8bea80
yogendra jain <yogendrajain64@gmail.com>
2022-07-23 16:58:22 +0530

    multiple

--- app.js
+++ app.js
@@ -5,33 +5,33 @@ var app = express();
 app.get("/", (req, res) => {
     var query = req.query;
     console.log(req.query);
-    res.send("+917340299380");
+    res.send("+917340299380,+919024191597");
 })
 
 app.get("/exotel", (req, res) => {
     var query = req.query;
     console.log(req.query);
     res.json({
-  "fetch_after_attempt": false,
-  "destination": {
-    "numbers": [
-      "+917424926949"
-    ]
-  },
-  "outgoing_phone_number": "01414931249",
-  "record": true,
-  "recording_channels": "dual",
-  "max_ringing_duration": 45,
-  "max_conversation_duration": 3600,
-  "music_on_hold": {
-    "type": "operator_tone"
-  },
-  "start_call_playback": {
-    "playback_to": "both",
-    "type": "text",
-    "value": "This text would be spoken out to the callee"
-  }
-});
+        "fetch_after_attempt": false,
+        "destination": {
+            "numbers": [
+                "+917424926949"
+            ]
+        },
+        "outgoing_phone_number": "01414931249",
+        "record": true,
+        "recording_channels": "dual",
+        "max_ringing_duration": 45,
+        "max_conversation_duration": 3600,
+        "music_on_hold": {
+            "type": "operator_tone"
+        },
+        "start_call_playback": {
+            "playback_to": "both",
+            "type": "text",
+            "value": "This text would be spoken out to the callee"
+        }
+    });
 })
 
 app.listen(5000, () => {
